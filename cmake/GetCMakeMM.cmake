a367a9592d60aca90a61e373e2bb7ccf5d92d550
flagarde <flagarde.programs@pm.me>
2024-03-05 13:47:35 +0800

    Move to CMMM

--- cmake/GetCMakeMM.cmake
+++ cmake/GetCMakeMM.cmake
@@ -1,92 +1,213 @@
+#
+# CMakeMM
+# CMake's missing Module Manager.
+#
+# SPDX-FileCopyrightText: 2023-2024 flagarde
+#
+# SPDX-License-Identifier: MIT
+#
+
+#[=======================================================================[.rst:
+cmmm
+----
+
+Download and configure CMakeMM
+
+.. code-block:: cmake
+
+  cmmm([NO_CHANGELOG] [NO_COLOR] [SHOW_PROGRESS] [VERSION <version>] [DESTINATION <path>] [INACTIVITY_TIMEOUT <seconds>] [TIMEOUT <seconds>] [TLS_VERIFY <ON|OFF>] [TLS_CAINFO <file>] [RETRIES <number/INFINITY>])
+
+Options
+^^^^^^^
+
+The options are:
+
+``NO_CHANGELOG``
+  **Optional** Disable changelog printing.
+
+``NO_COLOR``
+  **Optional** Disable the color on terminal.
+
+``SHOW_PROGRESS``
+  **Optional** Print progress information as status messages until the operation is complete.
+
+``VERSION <version>``
+  **Optional** Version of CMakeMM to download (use one of the versions in https://github.com/cmake-tools/cmmm/releases or 'latest' for the last version. Only for testing !).
+
+``DESTINATION <path>``
+  **Optional** Path destination to install CMakeMM.
+
+``INACTIVITY_TIMEOUT <seconds>``
+  **Optional** Terminate the operation after a period of inactivity.
+
+``TIMEOUT <seconds>``
+  **Optional** Terminate the operation after a given total time has elapsed.
+
+``TLS_VERIFY <ON|OFF>``
+  **Optional** Specify whether to verify the server certificate for ``https://`` URLs.
+  The default is to *not* verify. If this option is not specified, the value of the `CMAKE_TLS_VERIFY <https://cmake.org/cmake/help/latest/variable/CMAKE_TLS_VERIFY.html>`_ variable will be used instead.
+
+``TLS_CAINFO <file>``
+  **Optional** Specify a custom Certificate Authority file for ``https://`` URLs.
+  If this option is not specified, the value of the `CMAKE_TLS_CAINFO <https://cmake.org/cmake/help/latest/variable/CMAKE_TLS_CAINFO.html>`_ variable will be used instead.
+
+``RETRIES <number/INFINITY>``
+  **Optional** Specify the number of retries if download fails.
+
+#]=======================================================================]
+
 # cmake-format: off
-include_guard(GLOBAL)
+if(${CMAKE_VERSION} VERSION_GREATER "3.9.6")
+  include_guard(GLOBAL)
+endif()
 
-set(GET_CMMM_VERSION "1.0.0" CACHE INTERNAL "Version of GetCMakeMM.")
+set(GETCMMM_FILE_VERSION "1.0.0")
 
-# CMMM function
+if((NOT "${GETCMMM_FILE_VERSION}" VERSION_GREATER "${CURRENT_GETCMMM_FILE_VERSION}") AND COMMAND cmmm)
+  return()
+endif()
+
+set(CURRENT_GETCMMM_FILE_VERSION "${GETCMMM_FILE_VERSION}" CACHE INTERNAL "GetCMakeMM version.")
+unset(GETCMMM_FILE_VERSION)
+
+# Setup and download CMakeMM
 function(cmmm)
-  cmake_parse_arguments(CMMM "NO_COLOR" "VERSION;DESTINATION;INACTIVITY_TIMEOUT;TIMEOUT;REPOSITORY;PROVIDER" "" "${ARGN}")
+  if(${CMAKE_VERSION} VERSION_LESS "3.5")
+    include(CMakeParseArguments)
+  endif()
 
-  if(WIN32 OR DEFINED ENV{CLION_IDE} OR DEFINED ENV{DevEnvDir})
-    set(CMMM_NO_COLOR TRUE)
-  elseif(NOT DEFINED CMMM_NO_COLOR)
+  cmake_parse_arguments(CMMM "NO_COLOR;SHOW_PROGRESS" "VERSION;DESTINATION;INACTIVITY_TIMEOUT;TIMEOUT;TLS_VERIFY;TLS_CAINFO;RETRIES" "" "${ARGN}")
+
+  if(DEFINED ENV{CLICOLOR_FORCE} AND NOT "$ENV{CLICOLOR_FORCE}" STREQUAL "0")
     set(CMMM_NO_COLOR FALSE)
-  endif()
-  set_property(GLOBAL PROPERTY CMMM_NO_COLOR ${CMMM_NO_COLOR})
-
-  if(WIN32 OR (EXISTS $ENV{CLION_IDE}) OR (EXISTS $ENV{DevEnvDir}) OR (EXISTS $ENV{workspaceRoot}))
+  elseif(DEFINED ENV{CLICOLOR} AND "$ENV{CLICOLOR}" STREQUAL "0")
+    set(CMMM_NO_COLOR TRUE)
+  elseif(DEFINED ENV{CI} AND NOT CMMM_NO_COLOR)
+    set(CMMM_NO_COLOR FALSE)
+  elseif(WIN32 OR DEFINED ENV{DevEnvDir} OR DEFINED ENV{workspaceRoot} OR CMMM_NO_COLOR)
     set(CMMM_NO_COLOR TRUE)
   endif()
 
-  if(NOT DEFINED CMMM_VERSION)
-    set(CMMM_TAG "main")
-  elseif(CMMM_VERSION STREQUAL "main")
-    set(CMMM_TAG "${CMMM_VERSION}")
+  if(NOT CMMM_NO_COLOR)
+    set(CMMM_DEFAULT_COLORS "default=0;35:fatal_error=1;31:error=0;31:warn=0;33:info=0;32")
+    if(DEFINED ENV{CMMM_COLORS})
+      string(REPLACE ";" "." CMMM_COLORS "$ENV{CMMM_COLORS}")
+      string(REPLACE ":" ";" CMMM_COLORS "${CMMM_COLORS}")
+      string(REPLACE "=" ";" CMMM_COLORS "${CMMM_COLORS}")
+    elseif(DEFINED CMMM_COLORS)
+      string(REPLACE ";" "." CMMM_COLORS "${CMMM_COLORS}")
+      string(REPLACE ":" ";" CMMM_COLORS "${CMMM_COLORS}")
+      string(REPLACE "=" ";" CMMM_COLORS "${CMMM_COLORS}")
+    endif()
+    string(REPLACE ";" "." CMMM_DEFAULT_COLORS "${CMMM_DEFAULT_COLORS}")
+    string(REPLACE ":" ";" CMMM_DEFAULT_COLORS "${CMMM_DEFAULT_COLORS}")
+    string(REPLACE "=" ";" CMMM_DEFAULT_COLORS "${CMMM_DEFAULT_COLORS}")
+
+    set(COLOR_TYPES "default;fatal_error;error;warn;info")
+    cmake_parse_arguments(GIVEN "" "${COLOR_TYPES}" "" "${CMMM_COLORS}")
+    cmake_parse_arguments(DEFAULT "" "${COLOR_TYPES}" "" "${CMMM_DEFAULT_COLORS}")
+    foreach(COLOR_TYPE IN LISTS COLOR_TYPES)
+      string(TOUPPER "CMMM_${COLOR_TYPE}_COLOR" CMMM_TYPE_COLOR)
+      if(DEFINED GIVEN_${COLOR_TYPE})
+        string(REPLACE "." ";" GIVEN_${COLOR_TYPE} "${GIVEN_${COLOR_TYPE}}")
+        set(${CMMM_TYPE_COLOR} "[${GIVEN_${COLOR_TYPE}}m")
+      else()
+        string(REPLACE "." ";" DEFAULT_${COLOR_TYPE} "${DEFAULT_${COLOR_TYPE}}")
+        set(${CMMM_TYPE_COLOR} "[${DEFAULT_${COLOR_TYPE}}m")
+      endif()
+    endforeach()
+    string(ASCII 27 CMMM_ESC)
+    set(CMMM_RESET_COLOR "[0m")
+  endif()
+
+  if(NOT DEFINED CMMM_VERSION OR CMMM_VERSION STREQUAL "latest")
+    set(CMMM_URL "https://cmake-tools.github.io/cmmm")
+    set(CMMM_TAG "latest")
   else()
+    set(CMMM_URL "https://github.com/cmake-tools/cmmm/releases/download")
     set(CMMM_TAG "v${CMMM_VERSION}")
   endif()
 
   if(NOT DEFINED CMMM_DESTINATION)
-    set(CMMM_DESTINATION "${CMAKE_BINARY_DIR}")
+    set(CMMM_DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/cmmm")
   endif()
   get_filename_component(CMMM_DESTINATION "${CMMM_DESTINATION}" ABSOLUTE BASE_DIR "${CMAKE_BINARY_DIR}")
+  file(MAKE_DIRECTORY "${CMMM_DESTINATION}")
 
-  if(NOT DEFINED CMMM_INACTIVITY_TIMEOUT)
-    set(CMMM_INACTIVITY_TIMEOUT "5")
-  endif()
+  set(ARGN "${ARGN};TAG;${CMMM_TAG};DESTINATION;${CMMM_DESTINATION}")
 
-  if(NOT DEFINED CMMM_TIMEOUT)
-    set(CMMM_TIMEOUT "10")
-  endif()
-
-  string(ASCII 27 Esc)
-
-  if(NOT DEFINED CMMM_REPOSITORY)
-    set(CMMM_REPOSITORY "flagarde/CMakeMM")
-  endif()
-
-  if(NOT DEFINED CMMM_PROVIDER OR CMMM_PROVIDER STREQUAL "github")
-    set(CMMM_URL "https://raw.githubusercontent.com/${CMMM_REPOSITORY}")
-  elseif("${CMMM_PROVIDER}" STREQUAL "gitlab")
-    set(CMMM_URL "https://gitlab.com/${CMMM_REPOSITORY}/-/raw")
-  elseif("${CMMM_PROVIDER}" STREQUAL "gitee")
-    set(CMMM_URL "https://gitee.com/${CMMM_REPOSITORY}/raw")
-  else()
-    if(CMMM_NO_COLOR)
-      message("## [CMakeMM] Provider \"${CMMM_PROVIDER}\" unknown. Fallback to \"github\" ##")
-    else()
-      message("${Esc}[1;33m## [CMakeMM] Provider \"${CMMM_PROVIDER}\" unknown. Fallback to \"github\" ##${Esc}[m")
+  # Unlock file
+  function(unlock)
+    if(NOT CMAKE_VERSION VERSION_LESS 3.2)
+      file(LOCK "${CMMM_DESTINATION}/CMakeMM.cmake.lock" RELEASE)
     endif()
-    set(CMMM_URL "https://raw.githubusercontent.com/${CMMM_REPOSITORY}")
-  endif()
+  endfunction()
 
-  if(NOT CMAKEMM_INITIALIZED_${CMMM_TAG} OR NOT EXISTS "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake")
-
-    if(CMMM_NO_COLOR)
-      message("-- [CMakeMM] Downloading CMakeMM (${CMMM_TAG}) from ${CMMM_URL}/${CMMM_TAG}/CMakeMM.cmake to ${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake --")
-    else()
-      message("${Esc}[1;35m-- [CMakeMM] Downloading CMakeMM (${CMMM_TAG}) from ${CMMM_URL}/${CMMM_TAG}/CMakeMM.cmake ${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake --${Esc}[m")
-    endif()
-
-    file(DOWNLOAD "${CMMM_URL}/${CMMM_TAG}/CMakeMM.cmake" "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake" INACTIVITY_TIMEOUT "${CMMM_INACTIVITY_TIMEOUT}" LOG LOG_ STATUS CMAKECM_STATUS TIMEOUT "${CMMM_TIMEOUT}")
-    list(GET CMAKECM_STATUS 0 CMAKECM_CODE)
-    list(GET CMAKECM_STATUS 1 CMAKECM_MESSAGE)
-    if(${CMAKECM_CODE})
-      if(CMMM_NO_COLOR OR (CMAKE_VERSION VERSION_GREATER_EQUAL 3.21))
-        message(FATAL_ERROR "[CMakeMM] Error downloading CMakeMM.cmake : ${CMAKECM_MESSAGE}")
+  set(CMMM_COMMAND "")
+  set(ARGUMENTS "")
+  list(APPEND ARGUMENTS INACTIVITY_TIMEOUT TIMEOUT TLS_VERIFY TLS_CAINFO)
+  foreach(ARG IN LISTS ARGUMENTS)
+    if(DEFINED CMMM_${ARG})
+      if(NOT "${CMMM_COMMAND}" STREQUAL "")
+        set(CMMM_COMMAND "${CMMM_COMMAND};${ARG};${CMMM_${ARG}}")
       else()
-        message(FATAL_ERROR "${Esc}[31m[CMakeMM] Error downloading CMakeMM.cmake : ${CMAKECM_MESSAGE}${Esc}[m")
+        set(CMMM_COMMAND "${ARG};${CMMM_${ARG}}")
       endif()
     endif()
+  endforeach()
 
+  if(CMMM_SHOW_PROGRESS)
+    set(CMMM_COMMAND "${CMMM_COMMAND};SHOW_PROGRESS")
   endif()
 
+  if(NOT CMAKE_VERSION VERSION_LESS 3.2)
+    file(LOCK "${CMMM_DESTINATION}/CMakeMM.cmake.lock")
+  endif()
+
+  if(NOT DEFINED CMMM_RETRIES)
+    set(CMMM_RETRIES "0")
+  endif()
+  set(ARGN "${ARGN};RETRIES;${CMMM_RETRIES}")
+
+  if(EXISTS "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake")
+    file(SHA256 "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake" CMakeMMSHA256)
+  endif()
+
+  set(CMMM_EMPTY_FILE_SHA256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+  set(CMMM_RETRIES_DONE "0")
+  while(NOT CMAKEMM_INITIALIZED_${CMMM_TAG} OR NOT EXISTS "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake" OR "${CMakeMMSHA256}" STREQUAL "${CMMM_EMPTY_FILE_SHA256}")
+
+    if(${CMMM_RETRIES_DONE} STREQUAL "0")
+      message(STATUS "${CMMM_ESC}${CMMM_DEFAULT_COLOR}[ CMMM ] Downloading CMakeMM.cmake@${CMMM_VERSION} (${CMMM_TAG}) to ${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake${CMMM_ESC}${CMMM_RESET_COLOR}")
+    else()
+      message(STATUS "${CMMM_ESC}${CMMM_INFO_COLOR}[ CMMM ] Retry downloading CMakeMM.cmake (${CMMM_RETRIES_DONE}/${CMMM_RETRIES}).${CMMM_ESC}${CMMM_RESET_COLOR}")
+    endif()
+
+    file(DOWNLOAD "${CMMM_URL}/${CMMM_TAG}/CMakeMM.cmake" "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake" ${CMMM_COMMAND} LOG CMMM_LOG STATUS CMAKECM_STATUS)
+    list(GET CMAKECM_STATUS 0 CMAKECM_CODE)
+    list(GET CMAKECM_STATUS 1 CMAKECM_MESSAGE)
+    if(NOT "${CMAKECM_CODE}" STREQUAL "0")
+      message(STATUS "${CMMM_ESC}${CMMM_ERROR_COLOR}[ CMMM ] Error downloading CMakeMM.cmake : ${CMAKECM_MESSAGE} (${CMAKECM_CODE}).${CMMM_ESC}${CMMM_RESET_COLOR}")
+    else()
+      file(SHA256 "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake" CMakeMMSHA256)
+      if(${CMakeMMSHA256} STREQUAL "${CMMM_EMPTY_FILE_SHA256}")
+        file(REMOVE "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake")
+        message(STATUS "${CMMM_ESC}${CMMM_ERROR_COLOR}[ CMMM ] Error downloading CMakeMM.cmake : Empty file.${CMMM_ESC}${CMMM_RESET_COLOR}")
+      else()
+        break()
+      endif()
+    endif()
+    if("${CMMM_RETRIES_DONE}" STREQUAL "${CMMM_RETRIES}")
+      unlock()
+      message(STATUS "${CMMM_ESC}${CMMM_FATAL_ERROR_COLOR}[ CMMM ] Error downloading CMakeMM.cmake.${CMMM_ESC}${CMMM_RESET_COLOR}")
+      message(FATAL_ERROR "Error downloading CMakeMM.cmake.")
+    endif()
+    math(EXPR CMMM_RETRIES_DONE "${CMMM_RETRIES_DONE}+1")
+  endwhile()
+
   include("${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake")
-
-  set(ARGN "URL;${CMMM_URL};DESTINATION;${CMMM_DESTINATION};INACTIVITY_TIMEOUT;${CMMM_INACTIVITY_TIMEOUT};TIMEOUT;${CMMM_TIMEOUT};TAG;${CMMM_TAG};${ARGN}")
-  list(REMOVE_DUPLICATES ARGN)
-
-  cmmm_entry("${ARGN}")
+  cmmm_entry(${ARGN})
+  unlock()
 
 endfunction()
 # cmake-format: on
